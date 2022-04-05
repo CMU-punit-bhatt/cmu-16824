5a603abce25628b89edc5938b9b334aad95ae49f
Punit Bhatt <bhattpunit1309@gmail.com>
2022-04-05 03:43:34 -0400

    Adding code changes for the VAE section along with all the results, plots and images.

--- assignments/hw3/vae/train.py
+++ assignments/hw3/vae/train.py
@@ -10,10 +10,19 @@ import torch.nn.functional as F
 from torchvision.utils import make_grid
 from matplotlib import pyplot as plt
 import time
+import argparse
 import os
 from utils import *
 
-def ae_loss(model, x):
+train_loss_kl = []
+train_loss_recon = []
+train_loss_total = []
+
+val_loss_kl = []
+val_loss_recon = []
+val_loss_total = []
+
+def ae_loss(model, x, is_train=True):
     """
     TODO 2.1.2: fill in MSE loss between x and its reconstruction.
     return loss, {recon_loss = loss}
@@ -26,13 +35,21 @@ def ae_loss(model, x):
 
     loss = nn.MSELoss(reduction='sum')(out, x) / x.size(0)
 
+    if is_train:
+        train_loss_recon.append(loss.item())
+        train_loss_total.append(loss.item())
+    else:
+        val_loss_recon.append(loss.item())
+        val_loss_total.append(loss.item())
+
     return loss, OrderedDict(recon_loss=loss)
 
-def vae_loss(model, x, beta = 1):
+def vae_loss(model, x, beta = 1, is_train=True):
     """TODO 2.2.2 : Fill in recon_loss and kl_loss. """
 
     means, logstd = model.encoder(x)
-    out = model.decoder(means)
+    samples = means + torch.exp(logstd) * torch.randn(size=means.shape).cuda()
+    out = model.decoder(samples)
 
     vars = torch.exp(logstd) ** 2
     logvars = torch.log(vars)
@@ -40,9 +57,18 @@ def vae_loss(model, x, beta = 1):
     assert out.shape == x.shape
 
     recon_loss = nn.MSELoss(reduction='sum')(out, x) / x.size(0)
-    kl_loss = torch.sum(means ** 2 + vars - (logvars + 1), dim=1)
+    kl_loss = 0.5 * torch.mean(torch.sum(means ** 2 + vars - logvars - 1, dim=1))
 
     total_loss = recon_loss + beta * kl_loss
+
+    if is_train:
+        train_loss_recon.append(recon_loss.item())
+        train_loss_kl.append(kl_loss.item())
+        train_loss_total.append(total_loss.item())
+    else:
+        val_loss_recon.append(recon_loss.item())
+        val_loss_kl.append(kl_loss.item())
+        val_loss_total.append(total_loss.item())
 
     return total_loss, OrderedDict(recon_loss=recon_loss, kl_loss=kl_loss)
 
@@ -89,9 +115,9 @@ def get_val_metrics(model, loss_mode, val_loader):
         for x, _ in val_loader:
             x = preprocess_data(x)
             if loss_mode == 'ae':
-                _, _metric = ae_loss(model, x)
+                _, _metric = ae_loss(model, x, is_train=False)
             elif loss_mode == 'vae':
-                _, _metric = vae_loss(model, x)
+                _, _metric = vae_loss(model, x, is_train=False)
             all_metrics.append(_metric)
 
     return avg_dict(all_metrics)
@@ -101,6 +127,9 @@ def main(log_dir, loss_mode = 'vae', beta_mode = 'constant', num_epochs = 20, ba
 
     os.makedirs('data/'+ log_dir, exist_ok = True)
     train_loader, val_loader = get_dataloaders()
+
+    print('#batches train - ', len(train_loader))
+    print('#batches val - ', len(val_loader))
 
     variational = True if loss_mode == 'vae' else False
     model = AEModel(variational, latent_size, input_shape = (3, 32, 32)).cuda()
@@ -131,19 +160,81 @@ def main(log_dir, loss_mode = 'vae', beta_mode = 'constant', num_epochs = 20, ba
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model',
+                        help="Defines the model to train.",
+                        type=str,
+                        default=None)
+    parser.add_argument('--latent-dim',
+                        help="Defines the latent dim.",
+                        type=int,
+                        default=1024)
+    parser.add_argument('--beta',
+                        help="Defines the beta.",
+                        type=float,
+                        default=1)
+    args = parser.parse_args()
+    model = args.model
+    latent_dim = args.latent_dim
+    beta = args.beta
+
+    if model == 'ae_latent':
+        main('ae_latent' + str(latent_dim),
+             loss_mode = 'ae',  
+             num_epochs = 20, 
+             latent_size = latent_dim)
+
+    elif model == 'vae_latent':
+        main('vae_latent' + str(latent_dim), 
+             loss_mode = 'vae', 
+             num_epochs = 20, 
+             latent_size = latent_dim)
+
+    elif model == 'vae_latent1024_beta_constant':
+        main('vae_latent1024_beta_constant' + str(beta), 
+             loss_mode = 'vae', 
+             beta_mode = 'constant', 
+             target_beta_val = beta, 
+             num_epochs = 20, 
+             latent_size = 1024)
+
+    elif model == 'vae_latent1024_beta_linear1':
+        main('vae_latent1024_beta_linear1', 
+             loss_mode = 'vae', 
+             beta_mode = 'linear', 
+             target_beta_val = 1, 
+             num_epochs = 20, 
+             latent_size = 1024)
+
+    else:
+      print('You messed up :)')
+
+
     #TODO: Experiments to run :
     #2.1 - Auto-Encoder
     #Run for latent_sizes 16, 128 and 1024
-    #main('ae_latent16', loss_mode = 'ae',  num_epochs = 20, latent_size = 16)
-    #main('ae_latent128', loss_mode = 'ae',  num_epochs = 20, latent_size = 128)
-    #main('ae_latent1024', loss_mode = 'ae',  num_epochs = 20, latent_size = 1024)
+    # main('ae_latent16', loss_mode = 'ae',  num_epochs = 20, latent_size = 16)
+    # main('ae_latent128', loss_mode = 'ae',  num_epochs = 20, latent_size = 128)
+    # main('ae_latent1024', loss_mode = 'ae',  num_epochs = 20, latent_size = 1024)
 
     #Q 2.2 - Variational Auto-Encoder
-    #main('vae_latent1024', loss_mode = 'vae', num_epochs = 20, latent_size = 1024)
+    # main('vae_latent16', loss_mode = 'vae', num_epochs = 20, latent_size = 16)
+    # main('vae_latent128', loss_mode = 'vae', num_epochs = 20, latent_size = 128)
+    # main('vae_latent1024', loss_mode = 'vae', num_epochs = 20, latent_size = 1024)
 
     #Q 2.3.1 - Beta-VAE (constant beta)
     #Run for beta values 0.8, 1.2
-    #main('vae_latent1024_beta_constant0.8', loss_mode = 'vae', beta_mode = 'constant', target_beta_val = 0.8, num_epochs = 20, latent_size = 1024)
+    # main('vae_latent1024_beta_constant0.8', loss_mode = 'vae', beta_mode = 'constant', target_beta_val = 0.8, num_epochs = 20, latent_size = 1024)
+    # main('vae_latent1024_beta_constant1.2', loss_mode = 'vae', beta_mode = 'constant', target_beta_val = 1.2, num_epochs = 20, latent_size = 1024)
 
     #Q 2.3.2 - VAE with annealed beta (linear schedule)
-    #main('vae_latent1024_beta_linear1', loss_mode = 'vae', beta_mode = 'linear', target_beta_val = 1, num_epochs = 20, latent_size = 1024)
+    # main('vae_latent1024_beta_linear1', loss_mode = 'vae', beta_mode = 'linear', target_beta_val = 1, num_epochs = 20, latent_size = 1024)
+
+    print('Train Total:', train_loss_total)
+    print('Train recon:', train_loss_recon)
+    print('Train kl:', train_loss_kl)
+
+    print('Val Total:', val_loss_total)
+    print('Val recon:', val_loss_recon)
+    print('Val kl:', val_loss_kl)
